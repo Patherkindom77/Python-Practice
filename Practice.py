334b75b65ebbd23a39bac0172b387fce48a29de0
Patherkindom77 <tayarigabrieldavid@gmail.com>
2026-03-08 18:41:50 +0000

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -9,6 +9,13 @@ if num > 0:
 
 
 
-i = 4
+# Activity 2
 
-if ()
+actual_cost = float(input(" Please Enter the Actual Product Price: "))
+sale_amount = float(input("Please Enter the Sales Amount: "))
+
+if (sale_amount > actual_cost):
+    amount = sale_amount - actual_cost
+    print("Total Profit = {0}", format(amount))
+else:
+    print("No Profit!!!")
