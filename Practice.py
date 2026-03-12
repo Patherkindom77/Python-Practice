08083587d26b0b9925ab3647784301614ec2c137
Patherkindom77 <tayarigabrieldavid@gmail.com>
2026-03-12 19:24:08 +0000

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -1,21 +1,12 @@
-num = 3 
-if num > 0:
-    print(num, "is a positive number, ")
+a = 10
+b = -10
+c = 0
 
-
-num = -1 
-if num > 0:
-    print(num,"is a positive number, " )
-
-
-
-# Activity 2
-
-actual_cost = float(input(" Please Enter the Actual Product Price: "))
-sale_amount = float(input("Please Enter the Sales Amount: "))
-
-if (sale_amount > actual_cost):
-    amount = sale_amount - actual_cost
-    print("Total Profit = {0}", format(amount))
+if a > 0 or b > 0 :
+    print ("Either of the number is greater than 0")
 else:
-    print("No Profit!!!")
+    print("No number is greaer than 0")
+if b > 0 or c > 0: 
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
