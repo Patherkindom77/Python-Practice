b43d4743b85ae17fac5b415d4d8c7f43a2520203
Patherkindom77 <tayarigabrieldavid@gmail.com>
2026-03-12 19:44:19 +0000

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -10,3 +10,31 @@ if b > 0 or c > 0:
     print("Either of the number is greater than 0")
 else:
     print("No number is greater than 0")
+
+
+
+
+
+
+
+a = 10 
+b = 12 
+c = 12
+
+print(a != b)
+print(b != c)
+
+
+a = "python"
+b = "coding"
+
+if a != b :
+    print(a, 'and',b, 'are different.')
+
+
+
+
+a = int(input("enter a number"))
+
+if a%2 != 0 :
+    print(a, "is not an even number,") 
