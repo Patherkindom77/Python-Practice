e013373d57741eb5cabfbd6c6c9db771bb60b2bb
Patherkindom77 <tayarigabrieldavid@gmail.com>
2026-03-15 18:50:22 +0000

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -1,71 +1,14 @@
-a = 10
-b = -10
-c = 0
+a = [1,2,3,4,5]
 
-if a > 0 or b > 0 :
-    print ("Either of the number is greater than 0")
-else:
-    print("No number is greaer than 0")
-if b > 0 or c > 0: 
-    print("Either of the number is greater than 0")
-else:
-    print("No number is greater than 0")
+b = [1,2,3,4,5]
+
+c = a
 
 
+print(a is c)
 
+print(a is b)
 
+print( a is not c)
 
-
-
-a = 10 
-b = 12 
-c = 12
-
-print(a != b)
-print(b != c)
-
-
-a = "python"
-b = "coding"
-
-if a != b :
-    print(a, 'and',b, 'are different.')
-
-
-
-
-a = int(input("enter a number"))
-
-if a%2 != 0 :
-    print(a, "is not an even number,") 
-height = float(input("Enter your height in cm: "))
-
-weight = float(input("Enter your weight in kg: "))
-
-BMI = weight / (height/100)**2
-
-print("Your BMI is", BMI)
-
-if BMI <= 18.4:
-
-print("You are underweight.")
-
-elif BMI <= 24.9:
-
-print("You are healthy.")
-
-elif BMI <= 29.9:
-
-print("You are over weight.")
-
-elif BMI <= 34.9:
-
-print("You are severely over weight.")
-
-elif BMI <= 39.9:
-
-print("You are obese.")
-
-else: BMI <= 44.9
-
-print("You are severely obese.")
+print(a is not b)
