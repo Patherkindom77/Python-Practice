e8cd3f20decc28f6a98b4569c2ef4e8c772f5aad
Patherkindom77 <tayarigabrieldavid@gmail.com>
2026-04-05 19:57:48 +0100

    Update Nesting.py

--- Nesting.py
+++ Nesting.py
@@ -1,7 +1,16 @@
-# outer lamp
-for i in range(1,5):
-    # to iterate from 1 to 10
-    for j in range(1,11):
-        # dispay output
-        print(j, end=' ')
-    print()
+#Take input of a word
+
+string - input("Please enter your own word : ")
+char = input("Please enter your own Character :)
+i = 0
+court = 0
+#loop will find the occurence of character
+while(i < len(string)): # string operation
+
+    if(string[i] == char): #condiition 1
+        count = count + 1
+    i = i + 1
+
+    #Display the result 
+    print("The total Number of Times ",char, "has Occured = ",
+    court)
